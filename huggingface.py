db6b52941ac3f49db0dfdf1965f3366dc0d20f1e
Your Name <illahibuxjagirani@gmail.com>
2024-10-21 17:02:37 +0000

    class6

--- huggingface.py
+++ huggingface.py
@@ -1,11 +1,11 @@
 
-from langchain_huggingface import HuggingfaceEndpoint
+from langchain_huggingface import HuggingFaceEndpoint
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-llm = HuggingfaceEndpoint(
+llm = HuggingFaceEndpoint(
   repo_id="HuggingFaceH4/zephyr-7b-beta",
   huggingfacehub_api_token = os.getenv("HUGGINGFACE_API_TOKEN")
 )
